4d2abcd586726c0eed538584e3a009e4008b25ef
Isaaias-cloud <l22200712@pachuca.tecnm.mx>
2026-06-10 19:43:16 -0600

    Prueba

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // Copia aqui la configuracion web de Firebase Console.
 // Si dejas los valores vacios, la app funciona localmente con invitados y memoria del servidor.
-window.firebaseConfig = {
-  apiKey: 'AIzaSyCAzBzyjMyqo1VAaefpPnKSgIIlcSHSW5g',
-  authDomain: 'casino-online-256a7.firebaseapp.com',
-  projectId: 'casino-online-256a7',
-  storageBucket: 'casino-online-256a7.firebasestorage.app',
-  messagingSenderId: '534650445414',
-  appId: '1:534650445414:web:7032356201fd43e3ebd7a7'
-};
+// window.firebaseConfig = {
+//   apiKey: 'AIzaSyCAzBzyjMyqo1VAaefpPnKSgIIlcSHSW5g',
+//   authDomain: 'casino-online-256a7.firebaseapp.com',
+//   projectId: 'casino-online-256a7',
+//   storageBucket: 'casino-online-256a7.firebasestorage.app',
+//   messagingSenderId: '534650445414',
+//   appId: '1:534650445414:web:7032356201fd43e3ebd7a7'
+// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyCAzBzyjMyqo1VAaefpPnKSgIIlcSHSW5g",
@@ -16,6 +16,5 @@ const firebaseConfig = {
   projectId: "casino-online-256a7",
   storageBucket: "casino-online-256a7.firebasestorage.app",
   messagingSenderId: "534650445414",
-  appId: "1:534650445414:web:7032356201fd43e3ebd7a7",
-  measurementId: "G-NZLF5YL0V9"
+  appId: "1:534650445414:web:7032356201fd43e3ebd7a7"
 };
